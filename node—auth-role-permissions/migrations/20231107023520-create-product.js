f2d1963afe1ac38b0ad2d3a03cb2bcaefb5e61aa
tote2010 <gastonmascialino@yahoo.com.ar>
2023-11-08 08:38:11 -0300

    Primer commit

---
+++ node—auth-role-permissions/migrations/20231107023520-create-product.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      prod_name: {
+        type: Sequelize.INTEGER
+      },
+      prod_description: {
+        type: Sequelize.STRING
+      },
+      prod_image: {
+        type: Sequelize.STRING
+      },
+      prod_price: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
